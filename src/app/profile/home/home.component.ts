0e53337158ed5d6ab9417c5f18282cb2a470ff19
Yuda Ferry Mahendra <juncyboy@gmail.com>
2019-09-02 13:51:43 +0700

    add meta tag and title

--- src/app/profile/home/home.component.ts
+++ src/app/profile/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title, private metaService: Meta) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Yuda Ferry Mahendra | bealkha.com');
+    this.metaService.addTags([
+      { name: 'title', content: 'Yuda Ferry Mahendra | bealkha.com' },
+      { name: 'description', content: 'Yuda Ferry Mahendra, seorang web programmer sejak 2010. Saat ini mendalami nodejs, angular, ionic, dan mongodb.' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Yuda Ferry Mahendra' },
+      { name: 'keywords', content: 'Yuda Ferry Mahendra, Full stack programmer, Nodejs' },
+      { name: 'date', content: '2019-09-02', scheme: 'YYYY-MM-DD' },
+      { name: 'revisit-after', content: '7 days' },
+      { httpEquiv: 'Content-Type', content: 'text/html' },
+      { property: 'og:title', content: 'Yuda Ferry Mahendra | bealkha.com' },
+      { property: 'og:type', content: 'website' },
+      { charset: 'UTF-8' }
+    ], true);
   }
 
 }
