5ff2af0a6b279a71690cc69a4781c3c6ae3ffd1b
Yuda Ferry Mahendra <juncyboy@gmail.com>
2019-09-02 14:45:45 +0700

    fixing bug

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    // HomeComponent
   ],
   imports: [
     CommonModule,
